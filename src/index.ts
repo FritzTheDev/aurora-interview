058df25e90cc77d8860cc39d2338d7a1b05a44c3
Fritz <fritzj96@gmail.com>
2024-02-23 14:08:32 -0700

    environment variable validation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import "dotenv/config";
 import Express from "express";
+import { cleanEnv, str, port } from "envalid";
+
+cleanEnv(process.env, {
+  PORT: port({ default: 4200 }),
+  OPENAI_API_KEY: str(),
+});
 
 const app = Express();
 
